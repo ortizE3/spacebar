841744999e1527d68a9ac27ce6f88ba0e8f1d6c8
Ethan Ortiz <ortizethan3@gmail.com>
2025-11-17 19:47:20 -0600

    fixed video sizing

--- src/components/ProjectHero/ProjectHero.tsx
+++ src/components/ProjectHero/ProjectHero.tsx
@@ -5,14 +5,16 @@ import './ProjectHero.scss'
 function ProjectHero(props: ProjectHeroProps) {
     return (
         <>
-            <div className='flex flex-col items-center justify-center'>
-                <div className='hero-image-container'>
-                    <CloudflareVideo
-                        checkPageLoading
-                        videoSrc={props.videoSrc}
-                    />
-                </div>
-            </div >
+            {props.videoSrc.hlsSrc && props.videoSrc.dashSrc &&
+                <div className='flex flex-col items-center justify-center'>
+                    <div className='hero-image-container'>
+                        <CloudflareVideo
+                            checkPageLoading
+                            videoSrc={props.videoSrc}
+                        />
+                    </div>
+                </div >
+            }
         </>
 
     )
